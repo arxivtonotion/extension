bf8c3fdb94de6613d85575204c6ba4626b18c424
Priyansi <priyanhiman@gmail.com>
2021-06-16 13:39:57 +0530

    added template

--- background.js
+++ background.js
@@ -1,1 +1,5 @@
-
+chrome.runtime.onInstalled.addListener(async () => {
+    let url = chrome.runtime.getURL("test.html");
+    let tab = await chrome.tabs.create({ url });
+    console.log(`Created tab ${tab.id}`);
+});
